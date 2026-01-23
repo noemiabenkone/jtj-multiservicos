b5822e7cea8b0f353e8a2c11a548ddf0d4d74f94
Noêmia Benkone <noemiabenkone0@gmail.com>
2026-01-22 22:59:52 -0300

    feat: layout inicial da landing page da JTJ Multiserviços

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,82 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main className="bg-cover bg-center bg-[url(/images/imagem.png)] min-h-screen text-blue-950 ">
+      <header className=" flex items-center justify-around p-3 shadow-lg">
+        <h1 className="text-3xl ">
+          J<span className="text-green-600 text-3xl font-bold">T</span>J <span className="ml-2 font-semibold">Multiserviços</span>
+        </h1>
+        
+        <button className="bg-green-600 text-white hover:bg-green-500 px-4 py-2 rounded-lg font-medium transition cursor-pointer ">
+          Agendar agora
+        </button>
+      </header>
+
+      <section className="max-w-6xl mx-auto mt-10 p-10  rounded-xl shadow-sm ">
+        <div className="flex flex-col md:flex-row items-center justify-between gap-12">
+          <div className="text-left flex-1">
+            <h2 className="text-4xl lg:text-5xl font-extrabold text-blue-950 leading-tight">
+              Agência de Viagens<br/>e Assessoria Migratórios
+            </h2>
+            <span className="text-lg mt-2 block">
+               Cuidamos da sua viagem do início ao fim.
+            </span>
+            <p className="mt-6 text-lg font-semibold text-green-700">Transformamos exigências documentais em liberdade.</p>
+            <p className="text-blue-900 mt-2 leading-relaxed">
+              Oferecemos acompanhamento integral desde passagens e seguros até processos de naturalização e agendamentos oficiais. Cuidamos da sua jornada do início ao fim para garantir caminhos abertos e total tranquilidade.
+            </p>
+
+            <button className="bg-green-600 text-white px-6 py-3 mt-6 rounded-lg font-bold hover:bg-green-700 transition gap-2">
+              Quero Falar Com um Especialista Via WhatsApp
+            </button>
+          </div>
+    
+          
+          <div className="w-full md:w-[450px] h-[300px] relative rounded-2xl overflow-hidden shadow-2xl">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/images/image.png" 
+              alt="Consultoria em viagens e serviços migratórios"
+              fill 
+              className="object-cover" 
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
         </div>
-      </main>
-    </div>
+
+        
+        <div className="mt-20 flex flex-col items-center">
+          <h5 className="text-2xl font-bold border-b-4 border-green-600 pb-2 mb-10">
+            Nossos Serviços
+          </h5>
+          
+          
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
+            
+            <div className=" p-6 rounded-xl border border-zinc-200 hover:shadow-md transition">
+              <div className="text-3xl mb-4">✈️</div>
+              <h6 className="font-bold text-lg mb-2">Passagens Aéreas</h6>
+              <p className="text-sm text-blue-900">Emissão de passagens com as melhores rotas e tarifas.</p>
+            </div>
+
+            <div className=" p-6 rounded-xl border border-zinc-200 hover:shadow-md transition">
+              <div className="text-3xl mb-4">🪪</div>
+              <h6 className="font-bold text-lg mb-2">Naturalização</h6>
+              <p className="text-sm text-blue-900">Processos completos para sua cidadania e naturalização.</p>
+            </div>
+            <div className=" p-6 rounded-xl border border-zinc-200 hover:shadow-md transition">
+              <div className="text-3xl mb-4">🪪</div>
+              <h6 className="font-bold text-lg mb-2">Naturalização</h6>
+              <p className="text-sm text-blue-900">Processos completos para sua cidadania e naturalização.</p>
+            </div>
+            <div className=" p-6 rounded-xl border border-zinc-200 hover:shadow-md transition">
+              <div className="text-3xl mb-4">🪪</div>
+              <h6 className="font-bold text-lg mb-2">Naturalização</h6>
+              <p className="text-sm text-blue-900">Processos completos para sua cidadania e naturalização.</p>
+            </div>
+            
+            
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
